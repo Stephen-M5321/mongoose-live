8812167d97c2ce39180c5e79c0b2535a74f4d8b5
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-12 22:59:05 +0100

    get all books route added

--- src/server.js
+++ src/server.js
@@ -4,9 +4,50 @@ require("./db/connection");
 
 const express = require("express");
 
+const book = require("./books/model");
+
 const app = express();
 
 app.use(express.json());
+
+app.get("/books/getallbooks", async (req, res) => {
+    // res.send("Hello Me Beauty");              (used for testing)
+    try {
+    const books = await Book.find({});
+    console.log("books: ", typeof books, books);
+    const successResponse = {
+        message: "success",
+        books: books,
+    };
+
+    res.status(200).json(successResponse)   
+  } catch (error) {
+    console.log(error);  
+    }
+});
+
+app.post("/books/addbook", async (req, res) => {
+    // res.send("Hello from the book addbooks route");   (used for testing)
+    try{     
+        
+      const newBook =  await Book.create({
+        author: req.body.author,
+        genre: req.body.genre,
+        title: req.body.title,
+      });
+
+      const successResponse = {
+        messsage: "success",
+        newBook: newBook,
+      };
+
+      res.status(201).json(successResponse)
+    } catch(error) {
+      console.log(error);
+    }
+});
+
+
 
 // app.get("/books", (req, res) => {
 //     res.send("Hello world")
