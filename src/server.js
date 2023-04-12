0298bf831833e1ab8d32c90c0b9510afe9578ddb
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-12 20:10:54 +0100

    added design docs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
-const express = require("express")
+require("dotenv").config();
+require("./db/connection");
+// console.log(process.env.MONGO_URI);   (used for testing)
+
+const express = require("express");
 
 const app = express();
 
 app.use(express.json());
+
+// app.get("/books", (req, res) => {
+//     res.send("Hello world")
+// });
+// used for test on thunderclient
 
 app.listen(5001, () => console.log("server is listening"));
