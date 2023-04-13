a246d2a4792d7bc143246b9890845ed330ba9ef5
Stephen-M5321 <127414937+Stephen-M5321@users.noreply.github.com>
2023-04-13 19:24:29 +0100

    all up to date

--- src/server.js
+++ src/server.js
@@ -1,57 +1,72 @@
-require("dotenv").config();
-require("./db/connection");
-// console.log(process.env.MONGO_URI);   (used for testing)
 
-const express = require("express");
 
-const book = require("./books/model");
 
-const app = express();
 
-app.use(express.json());
+const express = require("express")
+require("dotenv").config()
+require("./db/connection")
+const Book = require("./modbooks/model")
+
+const app = express()
+
+app.use (express.json())
 
 app.get("/books/getallbooks", async (req, res) => {
-    // res.send("Hello Me Beauty");              (used for testing)
     try {
-    const books = await Book.find({});
-    console.log("books: ", typeof books, books);
-    const successResponse = {
-        message: "success",
-        books: books,
-    };
+        const books = await Book.find({});
+        // console.log("books: ", typeof books, books);      (used for testing)
+        const successResponse = {
+            message: "success",
+            books: books,
+        };
+    
+        res.status(200).json(successResponse)   
+      } catch (error) {
+        console.log(error);  
+        }
+})
 
-    res.status(200).json(successResponse)   
-  } catch (error) {
-    console.log(error);  
+app.put("/books/deletebook", async (request, response) => {
+    const bookList = await Book.deleteOne ({ 
+        // title: request.body.title,
+        // author: request.body.author,
+        title: "book1",
+        author: "jimmy matchMedia",
+    })
+    console.log(bookList)
+
+    const successSendResponse = {
+        message: "Deleted successfully",
+        book: bookList
     }
-});
+    // response.send(successSendResponse)
+    response.status(200).json(successSendResponse)
+})
+
+
+
 
 app.post("/books/addbook", async (req, res) => {
-    // res.send("Hello from the book addbooks route");   (used for testing)
-    try{     
-        
-      const newBook =  await Book.create({
-        author: req.body.author,
-        genre: req.body.genre,
-        title: req.body.title,
-      });
+    try{       
+        const newBook =  await Book.create({
+          author: req.body.author,
+          genre: req.body.genre,
+          title: req.body.title,
+        });
+  
+        // console.log(typeof newBook);                   (used for testing)
+        // console.log(newBook);                          (used for testing)
+  
+        const successResponse = {
+          messsage: "success",
+          newBook: newBook,
+        };
+  
+        res.status(201).json(successResponse)
+      } catch(error) {
+        console.log(error);
+      }
+} )
 
-      const successResponse = {
-        messsage: "success",
-        newBook: newBook,
-      };
+app.listen(5001, () => console.log("server is listening on 5001"))
 
-      res.status(201).json(successResponse)
-    } catch(error) {
-      console.log(error);
-    }
-});
-
-
-
-// app.get("/books", (req, res) => {
-//     res.send("Hello world")
-// });
-// used for test on thunderclient
-
-app.listen(5001, () => console.log("server is listening"));
